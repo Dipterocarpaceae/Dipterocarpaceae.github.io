eea1b72d3aad20a4fb1a850b176cef1751f96288
Dipterocarpaceae <bagusalmahenzar@gmail.com>
2025-05-10 18:44:15 +0700

    obfuscate js

--- js/contact.js
+++ js/contact.js
@@ -1,125 +1,192 @@
-// Contact form handler using EmailJS
-document.addEventListener("DOMContentLoaded", function () {
-  // Initialize EmailJS with your public key
-  // You'll need to sign up at https://www.emailjs.com/ and get your own public key
-  emailjs.init("4JW5-iQmneNc7OUGB"); // Replace with your actual public key after signing up
-
-  const contactForm = document.getElementById("contact-form");
-  const submitButton = document.querySelector(
-    '#contact-form button[type="submit"]'
+function _0x5e0c(_0x4fd724, _0x3bdf37) {
+  const _0x2c1dc9 = _0x2c1d();
+  return (
+    (_0x5e0c = function (_0x5e0c25, _0x24f4e5) {
+      _0x5e0c25 = _0x5e0c25 - 0x196;
+      let _0x4ca2c4 = _0x2c1dc9[_0x5e0c25];
+      return _0x4ca2c4;
+    }),
+    _0x5e0c(_0x4fd724, _0x3bdf37)
   );
-
-  if (contactForm) {
-    contactForm.addEventListener("submit", function (e) {
-      e.preventDefault();
-
-      // Get form data
-      const name = document.getElementById("name").value;
-      const email = document.getElementById("email").value;
-      const subject = document.getElementById("subject").value;
-      const message = document.getElementById("message").value;
-
-      // Basic validation
-      if (!name || !email || !subject || !message) {
-        showFormMessage("Please fill in all fields", "error");
-        return;
-      }
-
-      // Email validation
-      if (!isValidEmail(email)) {
-        showFormMessage("Please enter a valid email address", "error");
-        return;
-      }
-
-      // Disable submit button and show loading state
-      submitButton.disabled = true;
-      submitButton.innerHTML =
-        '<i class="fas fa-spinner fa-spin mr-2"></i> Sending...';
-
-      // Prepare template parameters for EmailJS
-      const templateParams = {
-        name: name,
-        email: email,
-        subject: subject,
-        message: message,
-        to_email: "bagusalmahenzar@gmail.com",
-      };
-
-      // Send email using EmailJS
-      emailjs
-        .send("service_9ag5d3g", "template_wcsh828", templateParams) // Replace with your service ID and template ID
-        .then(function (response) {
-          console.log("SUCCESS!", response.status, response.text);
-          showFormMessage(
-            "Your message has been sent successfully!",
-            "success"
-          );
-          contactForm.reset();
-        })
-        .catch(function (error) {
-          console.log("FAILED...", error);
-          showFormMessage(
-            "Failed to send message. Please try again later.",
-            "error"
-          );
-        })
-        .finally(function () {
-          // Re-enable submit button
-          submitButton.disabled = false;
-          submitButton.innerHTML = "Send Message";
-        });
-    });
-  }
-
-  // Helper function to validate email
-  function isValidEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
-
-  // Helper function to show form messages
-  function showFormMessage(message, type) {
-    const messageElement = document.getElementById("form-message");
-
-    if (messageElement) {
-      messageElement.textContent = message;
-      messageElement.className = "mt-4 text-center";
-
-      if (type === "error") {
-        messageElement.classList.add("text-red-600");
-      } else if (type === "success") {
-        messageElement.classList.add("text-green-600");
-      }
-
-      // Clear message after 5 seconds
-      setTimeout(() => {
-        messageElement.textContent = "";
-        messageElement.className = "mt-4 text-center";
-      }, 5000);
-    } else {
-      // If message element doesn't exist, create one
-      const newMessageElement = document.createElement("div");
-      newMessageElement.id = "form-message";
-      newMessageElement.className = "mt-4 text-center";
-      newMessageElement.textContent = message;
-
-      if (type === "error") {
-        newMessageElement.classList.add("text-red-600");
-      } else if (type === "success") {
-        newMessageElement.classList.add("text-green-600");
-      }
-
-      // Insert after the form
-      contactForm.parentNode.insertBefore(
-        newMessageElement,
-        contactForm.nextSibling
-      );
-
-      // Clear message after 5 seconds
-      setTimeout(() => {
-        newMessageElement.textContent = "";
-        newMessageElement.className = "mt-4 text-center";
-      }, 5000);
+}
+function _0x2c1d() {
+  const _0x33ed77 = [
+    "submit",
+    "2617056xJKLnt",
+    "text-red-600",
+    "DOMContentLoaded",
+    "status",
+    "add",
+    "FAILED...",
+    "value",
+    "1351416LeVfXW",
+    "send",
+    "text-green-600",
+    "4JW5-iQmneNc7OUGB",
+    "1309548JsWLaF",
+    "8227303OUXPIa",
+    "Failed\x20to\x20send\x20message.\x20Please\x20try\x20again\x20later.",
+    "mt-4\x20text-center",
+    "<i\x20class=\x22fas\x20fa-spinner\x20fa-spin\x20mr-2\x22></i>\x20Sending...",
+    "then",
+    "Your\x20message\x20has\x20been\x20sent\x20successfully!",
+    "service_9ag5d3g",
+    "61942UIJdQy",
+    "preventDefault",
+    "template_wcsh828",
+    "className",
+    "querySelector",
+    "reset",
+    "catch",
+    "form-message",
+    "createElement",
+    "success",
+    "error",
+    "textContent",
+    "621990ZJvzKW",
+    "1368320xcavav",
+    "23HJTPrh",
+    "disabled",
+    "div",
+    "Please\x20fill\x20in\x20all\x20fields",
+    "init",
+    "text",
+    "subject",
+    "addEventListener",
+    "SUCCESS!",
+    "10JXpOVw",
+    "log",
+    "getElementById",
+    "bagusalmahenzar@gmail.com",
+    "insertBefore",
+    "contact-form",
+    "classList",
+    "innerHTML",
+  ];
+  _0x2c1d = function () {
+    return _0x33ed77;
+  };
+  return _0x2c1d();
+}
+const _0x37de22 = _0x5e0c;
+(function (_0x4c1347, _0xeffd1a) {
+  const _0x4e9a36 = _0x5e0c,
+    _0x236c49 = _0x4c1347();
+  while (!![]) {
+    try {
+      const _0x498050 =
+        (parseInt(_0x4e9a36(0x19e)) / 0x1) *
+          (parseInt(_0x4e9a36(0x1c3)) / 0x2) +
+        parseInt(_0x4e9a36(0x1b0)) / 0x3 +
+        -parseInt(_0x4e9a36(0x19d)) / 0x4 +
+        (parseInt(_0x4e9a36(0x1a7)) / 0x5) *
+          (parseInt(_0x4e9a36(0x1bb)) / 0x6) +
+        -parseInt(_0x4e9a36(0x1bc)) / 0x7 +
+        parseInt(_0x4e9a36(0x1b7)) / 0x8 +
+        parseInt(_0x4e9a36(0x19c)) / 0x9;
+      if (_0x498050 === _0xeffd1a) break;
+      else _0x236c49["push"](_0x236c49["shift"]());
+    } catch (_0x4d9169) {
+      _0x236c49["push"](_0x236c49["shift"]());
     }
   }
-});
+})(_0x2c1d, 0xb51c5),
+  document[_0x37de22(0x1a5)](_0x37de22(0x1b2), function () {
+    const _0x3df124 = _0x37de22;
+    emailjs[_0x3df124(0x1a2)](_0x3df124(0x1ba));
+    const _0x49b41d = document[_0x3df124(0x1a9)](_0x3df124(0x1ac)),
+      _0x49c98a = document[_0x3df124(0x1c7)](
+        "#contact-form\x20button[type=\x22submit\x22]"
+      );
+    _0x49b41d &&
+      _0x49b41d[_0x3df124(0x1a5)](_0x3df124(0x1af), function (_0x397471) {
+        const _0x640bb9 = _0x3df124;
+        _0x397471[_0x640bb9(0x1c4)]();
+        const _0x25473e = document[_0x640bb9(0x1a9)]("name")[_0x640bb9(0x1b6)],
+          _0x379868 = document[_0x640bb9(0x1a9)]("email")["value"],
+          _0x1b21e6 = document[_0x640bb9(0x1a9)](_0x640bb9(0x1a4))["value"],
+          _0x414436 = document["getElementById"]("message")[_0x640bb9(0x1b6)];
+        if (!_0x25473e || !_0x379868 || !_0x1b21e6 || !_0x414436) {
+          _0x30bc1(_0x640bb9(0x1a1), "error");
+          return;
+        }
+        if (!_0xc99c8(_0x379868)) {
+          _0x30bc1(
+            "Please\x20enter\x20a\x20valid\x20email\x20address",
+            _0x640bb9(0x19a)
+          );
+          return;
+        }
+        (_0x49c98a[_0x640bb9(0x19f)] = !![]),
+          (_0x49c98a[_0x640bb9(0x1ae)] = _0x640bb9(0x1bf));
+        const _0x17380b = {
+          name: _0x25473e,
+          email: _0x379868,
+          subject: _0x1b21e6,
+          message: _0x414436,
+          to_email: _0x640bb9(0x1aa),
+        };
+        emailjs[_0x640bb9(0x1b8)](_0x640bb9(0x1c2), _0x640bb9(0x1c5), _0x17380b)
+          [_0x640bb9(0x1c0)](function (_0x98762f) {
+            const _0x42ad4e = _0x640bb9;
+            console["log"](
+              _0x42ad4e(0x1a6),
+              _0x98762f[_0x42ad4e(0x1b3)],
+              _0x98762f[_0x42ad4e(0x1a3)]
+            ),
+              _0x30bc1(_0x42ad4e(0x1c1), _0x42ad4e(0x199)),
+              _0x49b41d[_0x42ad4e(0x1c8)]();
+          })
+          [_0x640bb9(0x196)](function (_0x43db8e) {
+            const _0x5be35e = _0x640bb9;
+            console[_0x5be35e(0x1a8)](_0x5be35e(0x1b5), _0x43db8e),
+              _0x30bc1(_0x5be35e(0x1bd), _0x5be35e(0x19a));
+          })
+          ["finally"](function () {
+            const _0xf8aaff = _0x640bb9;
+            (_0x49c98a[_0xf8aaff(0x19f)] = ![]),
+              (_0x49c98a[_0xf8aaff(0x1ae)] = "Send\x20Message");
+          });
+      });
+    function _0xc99c8(_0x67f3d3) {
+      const _0x2d2afa = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return _0x2d2afa["test"](_0x67f3d3);
+    }
+    function _0x30bc1(_0x45aaa6, _0x92978c) {
+      const _0x15adcc = _0x3df124,
+        _0x4eac7f = document["getElementById"](_0x15adcc(0x197));
+      if (_0x4eac7f) {
+        (_0x4eac7f[_0x15adcc(0x19b)] = _0x45aaa6),
+          (_0x4eac7f[_0x15adcc(0x1c6)] = "mt-4\x20text-center");
+        if (_0x92978c === _0x15adcc(0x19a))
+          _0x4eac7f[_0x15adcc(0x1ad)][_0x15adcc(0x1b4)](_0x15adcc(0x1b1));
+        else
+          _0x92978c === _0x15adcc(0x199) &&
+            _0x4eac7f[_0x15adcc(0x1ad)][_0x15adcc(0x1b4)](_0x15adcc(0x1b9));
+        setTimeout(() => {
+          const _0x4692aa = _0x15adcc;
+          (_0x4eac7f[_0x4692aa(0x19b)] = ""),
+            (_0x4eac7f[_0x4692aa(0x1c6)] = _0x4692aa(0x1be));
+        }, 0x1388);
+      } else {
+        const _0x2e8077 = document[_0x15adcc(0x198)](_0x15adcc(0x1a0));
+        (_0x2e8077["id"] = _0x15adcc(0x197)),
+          (_0x2e8077[_0x15adcc(0x1c6)] = "mt-4\x20text-center"),
+          (_0x2e8077[_0x15adcc(0x19b)] = _0x45aaa6);
+        if (_0x92978c === _0x15adcc(0x19a))
+          _0x2e8077[_0x15adcc(0x1ad)][_0x15adcc(0x1b4)](_0x15adcc(0x1b1));
+        else
+          _0x92978c === _0x15adcc(0x199) &&
+            _0x2e8077[_0x15adcc(0x1ad)]["add"](_0x15adcc(0x1b9));
+        _0x49b41d["parentNode"][_0x15adcc(0x1ab)](
+          _0x2e8077,
+          _0x49b41d["nextSibling"]
+        ),
+          setTimeout(() => {
+            const _0x15f000 = _0x15adcc;
+            (_0x2e8077[_0x15f000(0x19b)] = ""),
+              (_0x2e8077[_0x15f000(0x1c6)] = _0x15f000(0x1be));
+          }, 0x1388);
+      }
+    }
+  });
--- js/projects.js
+++ js/projects.js
@@ -1,104 +1,184 @@
-// Function to fetch projects from data.json
+const _0x1c5c32 = _0x46af;
+(function (_0x58190e, _0x5ba289) {
+  const _0x1aae4e = _0x46af,
+    _0x4dcb12 = _0x58190e();
+  while (!![]) {
+    try {
+      const _0x5d10ca =
+        (-parseInt(_0x1aae4e(0xfb)) / 0x1) *
+          (-parseInt(_0x1aae4e(0x129)) / 0x2) +
+        -parseInt(_0x1aae4e(0xfc)) / 0x3 +
+        parseInt(_0x1aae4e(0x125)) / 0x4 +
+        parseInt(_0x1aae4e(0x114)) / 0x5 +
+        (parseInt(_0x1aae4e(0xf9)) / 0x6) *
+          (-parseInt(_0x1aae4e(0x116)) / 0x7) +
+        (-parseInt(_0x1aae4e(0x10c)) / 0x8) *
+          (-parseInt(_0x1aae4e(0x117)) / 0x9) +
+        (-parseInt(_0x1aae4e(0x101)) / 0xa) * (parseInt(_0x1aae4e(0xfe)) / 0xb);
+      if (_0x5d10ca === _0x5ba289) break;
+      else _0x4dcb12["push"](_0x4dcb12["shift"]());
+    } catch (_0x3c99cb) {
+      _0x4dcb12["push"](_0x4dcb12["shift"]());
+    }
+  }
+})(_0x29c9, 0x63421);
 async function fetchProjects() {
-    try {
-        const response = await fetch('/data.json');
-        if (!response.ok) {
-            throw new Error('Failed to fetch projects data');
-        }
-        const data = await response.json();
-        return data.projects;
-    } catch (error) {
-        console.error('Error loading projects:', error);
-        return [];
-    }
+  const _0x535ae1 = _0x46af;
+  try {
+    const _0x433012 = await fetch(_0x535ae1(0x12e));
+    if (!_0x433012["ok"]) throw new Error(_0x535ae1(0x113));
+    const _0x2dd129 = await _0x433012[_0x535ae1(0x105)]();
+    return _0x2dd129["projects"];
+  } catch (_0x7dd94) {
+    return console[_0x535ae1(0x121)](_0x535ae1(0x110), _0x7dd94), [];
+  }
 }
-
-// Function to create a project card element
-function createProjectCard(project, delay) {
-    const projectCard = document.createElement('div');
-    projectCard.className = `project-card bg-white rounded-lg overflow-hidden shadow-md animate-fade-in delay-${delay}`;
-
-    // Create the HTML structure for the project card
-    projectCard.innerHTML = `
-        <div class="h-48 overflow-hidden">
-            <img src="/img/projects/${project.image}" alt="${project.title}"
-                class="w-full h-full object-cover transition-transform duration-500 hover:scale-105">
-        </div>
-        <div class="p-6">
-            <h3 class="text-xl font-semibold mb-2">${project.title}</h3>
-            <p class="text-gray-600 mb-4">${project.description}</p>
-
-            <div class="flex flex-wrap gap-2 mb-4">
-                ${project.technologies.map(tech =>
-                    `<span class="tech-tag bg-${tech.color}-100 text-${tech.color}-800 px-3 py-1 rounded-full text-sm">${tech.name}</span>`
-                ).join('')}
-            </div>
-
-            <div class="flex space-x-3">
-                ${project.liveLink ?
-                    `<a href="${project.liveLink}" target="_blank" class="text-indigo-600 hover:text-indigo-800 transition">
-                        <i class="fas fa-external-link-alt"></i> Live
-                    </a>` : ''}
-                ${project.sourceLink ?
-                    `<a href="${project.sourceLink}" class="text-gray-600 hover:text-indigo-600 transition">
-                        <i class="fab fa-github"></i> Source Code
-                    </a>` : ''}
-            </div>
-        </div>
-    `;
-
-    return projectCard;
+function createProjectCard(_0xf850b, _0x100074) {
+  const _0x1309c8 = _0x46af,
+    _0x4b732f = document["createElement"](_0x1309c8(0x109));
+  return (
+    (_0x4b732f[_0x1309c8(0x11c)] =
+      "project-card\x20bg-white\x20rounded-lg\x20overflow-hidden\x20shadow-md\x20animate-fade-in\x20delay-" +
+      _0x100074),
+    (_0x4b732f[_0x1309c8(0x127)] =
+      _0x1309c8(0x103) +
+      _0xf850b["image"] +
+      _0x1309c8(0x104) +
+      _0xf850b[_0x1309c8(0xfd)] +
+      "\x22\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20class=\x22w-full\x20h-full\x20object-cover\x20transition-transform\x20duration-500\x20hover:scale-105\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22p-6\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<h3\x20class=\x22text-xl\x20font-semibold\x20mb-2\x22>" +
+      _0xf850b["title"] +
+      _0x1309c8(0x124) +
+      _0xf850b[_0x1309c8(0x10f)] +
+      _0x1309c8(0x123) +
+      _0xf850b[_0x1309c8(0x122)]
+        [_0x1309c8(0xff)](
+          (_0x43225f) =>
+            _0x1309c8(0x118) +
+            _0x43225f[_0x1309c8(0x12b)] +
+            _0x1309c8(0x12c) +
+            _0x43225f["color"] +
+            _0x1309c8(0x111) +
+            _0x43225f[_0x1309c8(0x11d)] +
+            _0x1309c8(0x100)
+        )
+        [_0x1309c8(0x11a)]("") +
+      "\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22flex\x20space-x-3\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20" +
+      (_0xf850b[_0x1309c8(0x102)]
+        ? _0x1309c8(0x115) + _0xf850b[_0x1309c8(0x102)] + _0x1309c8(0x120)
+        : "") +
+      _0x1309c8(0x11b) +
+      (_0xf850b["sourceLink"]
+        ? "<a\x20href=\x22" + _0xf850b[_0x1309c8(0x108)] + _0x1309c8(0x11e)
+        : "") +
+      "\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20"),
+    _0x4b732f
+  );
 }
-
-// Function to render projects in the projects section
+function _0x46af(_0x33d8e0, _0x483cab) {
+  const _0x29c9d3 = _0x29c9();
+  return (
+    (_0x46af = function (_0x46af57, _0x376d56) {
+      _0x46af57 = _0x46af57 - 0xf9;
+      let _0x3daa6e = _0x29c9d3[_0x46af57];
+      return _0x3daa6e;
+    }),
+    _0x46af(_0x33d8e0, _0x483cab)
+  );
+}
+function _0x29c9() {
+  const _0x54bb0e = [
+    "title",
+    "6743tjShKa",
+    "map",
+    "</span>",
+    "14230vcyNzd",
+    "liveLink",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22h-48\x20overflow-hidden\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<img\x20src=\x22/img/projects/",
+    "\x22\x20alt=\x22",
+    "json",
+    "createElement",
+    "getElementById",
+    "sourceLink",
+    "div",
+    "<p\x20class=\x22text-gray-600\x22>No\x20projects\x20found.</p>",
+    "Error\x20rendering\x20projects:",
+    "856312BLAsvQ",
+    "querySelector",
+    "projects-loading",
+    "description",
+    "Error\x20loading\x20projects:",
+    "-800\x20px-3\x20py-1\x20rounded-full\x20text-sm\x22>",
+    "remove",
+    "Failed\x20to\x20fetch\x20projects\x20data",
+    "2211075KANsxw",
+    "<a\x20href=\x22",
+    "336455MohFUi",
+    "54OgQCFP",
+    "<span\x20class=\x22tech-tag\x20bg-",
+    "addEventListener",
+    "join",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+    "className",
+    "name",
+    "\x22\x20class=\x22text-gray-600\x20hover:text-indigo-600\x20transition\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<i\x20class=\x22fab\x20fa-github\x22></i>\x20Source\x20Code\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</a>",
+    "Projects\x20container\x20not\x20found",
+    "\x22\x20target=\x22_blank\x22\x20class=\x22text-indigo-600\x20hover:text-indigo-800\x20transition\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<i\x20class=\x22fas\x20fa-external-link-alt\x22></i>\x20Live\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</a>",
+    "error",
+    "technologies",
+    "</p>\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22flex\x20flex-wrap\x20gap-2\x20mb-4\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+    "</h3>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p\x20class=\x22text-gray-600\x20mb-4\x22>",
+    "2386684WBigKZ",
+    "forEach",
+    "innerHTML",
+    "DOMContentLoaded",
+    "2IXJmji",
+    "appendChild",
+    "color",
+    "-100\x20text-",
+    "col-span-3\x20text-center\x20py-12",
+    "/data.json",
+    "66sjhjUR",
+    "#projects\x20.grid",
+    "727161uAEyyY",
+    "1802118zIUBTh",
+  ];
+  _0x29c9 = function () {
+    return _0x54bb0e;
+  };
+  return _0x29c9();
+}
 async function renderProjects() {
-    const projectsContainer = document.querySelector('#projects .grid');
-    if (!projectsContainer) {
-        console.error('Projects container not found');
-        return;
+  const _0x4982d6 = _0x46af,
+    _0x26e3b8 = document[_0x4982d6(0x10d)](_0x4982d6(0xfa));
+  if (!_0x26e3b8) {
+    console["error"](_0x4982d6(0x11f));
+    return;
+  }
+  const _0x29ea64 = document[_0x4982d6(0x107)](_0x4982d6(0x10e));
+  try {
+    const _0x3ee70c = await fetchProjects();
+    _0x29ea64 && _0x29ea64[_0x4982d6(0x112)]();
+    if (_0x3ee70c["length"] === 0x0) {
+      const _0x33b6bd = document[_0x4982d6(0x106)]("div");
+      (_0x33b6bd[_0x4982d6(0x11c)] = _0x4982d6(0x12d)),
+        (_0x33b6bd[_0x4982d6(0x127)] = _0x4982d6(0x10a)),
+        _0x26e3b8[_0x4982d6(0x12a)](_0x33b6bd);
+      return;
     }
-
-    // Show loading indicator
-    const loadingElement = document.getElementById('projects-loading');
-
-    try {
-        // Fetch projects from data.json
-        const projects = await fetchProjects();
-
-        // Remove loading indicator
-        if (loadingElement) {
-            loadingElement.remove();
-        }
-
-        // If no projects found, show a message
-        if (projects.length === 0) {
-            const noProjectsMessage = document.createElement('div');
-            noProjectsMessage.className = 'col-span-3 text-center py-12';
-            noProjectsMessage.innerHTML = '<p class="text-gray-600">No projects found.</p>';
-            projectsContainer.appendChild(noProjectsMessage);
-            return;
-        }
-
-        // Create and append project cards
-        projects.forEach((project, index) => {
-            const delay = (index % 3) * 100 + 100; // Calculate delay based on index
-            const projectCard = createProjectCard(project, delay);
-            projectsContainer.appendChild(projectCard);
-        });
-    } catch (error) {
-        console.error('Error rendering projects:', error);
-
-        // Remove loading indicator and show error message
-        if (loadingElement) {
-            loadingElement.remove();
-        }
-
-        const errorMessage = document.createElement('div');
-        errorMessage.className = 'col-span-3 text-center py-12';
-        errorMessage.innerHTML = '<p class="text-red-600">Failed to load projects. Please try again later.</p>';
-        projectsContainer.appendChild(errorMessage);
-    }
+    _0x3ee70c[_0x4982d6(0x126)]((_0x17e2c2, _0x4323aa) => {
+      const _0x1bf6e7 = _0x4982d6,
+        _0x7a376f = (_0x4323aa % 0x3) * 0x64 + 0x64,
+        _0x21a352 = createProjectCard(_0x17e2c2, _0x7a376f);
+      _0x26e3b8[_0x1bf6e7(0x12a)](_0x21a352);
+    });
+  } catch (_0x561b5e) {
+    console["error"](_0x4982d6(0x10b), _0x561b5e);
+    _0x29ea64 && _0x29ea64[_0x4982d6(0x112)]();
+    const _0x14e6dd = document["createElement"](_0x4982d6(0x109));
+    (_0x14e6dd["className"] = _0x4982d6(0x12d)),
+      (_0x14e6dd[_0x4982d6(0x127)] =
+        "<p\x20class=\x22text-red-600\x22>Failed\x20to\x20load\x20projects.\x20Please\x20try\x20again\x20later.</p>"),
+      _0x26e3b8[_0x4982d6(0x12a)](_0x14e6dd);
+  }
 }
-
-// Initialize projects when the DOM is loaded
-document.addEventListener('DOMContentLoaded', renderProjects);
+document[_0x1c5c32(0x119)](_0x1c5c32(0x128), renderProjects);
